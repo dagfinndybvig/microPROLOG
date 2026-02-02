afafdadf0c1464dd24ae1643fdd16f878fac4025
dagfinndybvig <dagfinn.dybvig@gmail.com>
2026-02-02 15:32:37 +0100

    Remove redundant test files
    Removed temporary test files:
    - family2.pl (saved session file)
    - simple_builtins_test.pl (superseded by builtins_test.pl)
    
    Kept builtins_test.pl as a comprehensive example/test suite

--- simple_builtins_test.pl
+++
@@ -1,9 +0,0 @@
-% Simple test file for built-ins in rules
-% Each rule must be on a single line
-
-(value five 5)
-(value ten 10)
-
-((double X Result) (is Result (* X 2)))
-((add X Y Result) (is Result (+ X Y)))
-((isEven X) (is Remainder (- X (* (/ X 2) 2))) (= Remainder 0))
