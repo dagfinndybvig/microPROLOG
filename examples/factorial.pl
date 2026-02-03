fe896c14725fdcd74be63e22f09bd8599619d0ec
dagfinndybvig <dagfinn.dybvig@gmail.com>
2026-02-03 14:58:10 +0100

    Simplify factorial by removing redundant guard
    - Removed (> N 0) check for cleaner code
    - Matches pure mathematical definition more closely
    - Guard was defensive against absurd negative input
    - Result is more elegant and pedagogically clear

--- examples/factorial.pl
+++ examples/factorial.pl
@@ -1,13 +1,12 @@
 % Factorial computation using recursion
-% Demonstrates arithmetic, comparison, and cut operators
+% Demonstrates arithmetic and cut operators
 
 % Base case: factorial of 0 is 1
 % Cut prevents backtracking once we match N = 0
 ((fact 0 1) !).
 
 % Recursive case: fact(N) = N * fact(N-1)
-% Only tries this if N > 0 (first clause didn't match)
-((fact N Result) (> N 0) (is N1 (- N 1)) (fact N1 R1) (is Result (* N R1))).
+((fact N Result) (is N1 (- N 1)) (fact N1 R1) (is Result (* N R1))).
 
 % Example queries:
 % ? (fact 5 X)
